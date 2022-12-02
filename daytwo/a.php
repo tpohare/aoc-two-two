d7781a47c5893a96b3f0af07d86419d1e54f3aae
tpohare <github@hobocore.info>
2022-12-02 20:44:00 +0000

    Update a.php

--- daytwo/a.php
+++ daytwo/a.php
@@ -9,37 +9,13 @@ class GameRules {
             $X = "rock",
             $Y = "paper",
             $Z = "scissors";
-    private $game_points;
 
-    public function __construct()
-    {
-        $rock_score = 1;
-        $paper_score = 2;
-        $scissors_score = 3;
-        $win_score = 6;
-        $loss_score = 0;
-        $draw_score = 3;
-
-        $this -> game_points = [//You throw Rock
-            'rock' => [// I throw
-                'rock' => $rock_score + $draw_score, // Draw
-                'paper' => $paper_score + $win_score, // Paper covers rock
-                'scissors' => $scissors_score + $loss_score, // Rock blunts scissors
-            ],
-            //You throw Paper
-            'paper' => [// I throw
-                'rock' => $rock_score + $loss_score, // Paper covers rock
-                'paper' => $paper_score + $draw_score, // Draw
-                'scissors' => $scissors_score + $win_score, // Scissors cuts paper
-            ],
-            //You throw Scissors
-            'scissors' => [// I throw
-                'rock' => $rock_score + $win_score, // Paper covers rock
-                'paper' => $paper_score + $loss_score, // Scissors cuts paper
-                'scissors' => $scissors_score + $draw_score, // Draw],
-            ]
-        ];
-    }
+    private $rock_score = 1,
+            $paper_score = 2,
+            $scissors_score = 3,
+            $win_score = 6,
+            $loss_score = 0,
+            $draw_score = 3;
 
     public function totalScores($input, $_this) {
         return array_reduce(explode("\n", $input), static function($running_total, $game_result) use ($_this) {
@@ -48,11 +24,23 @@ class GameRules {
     }
 
     private function calculateScore($game_result) {
-        $result = explode(" ", $game_result);
-        $you = $this -> {$result[0]};
-        $me = $this -> {$result[1]};
+        return $this -> scores(
+            sprintf("%s|%s", $this -> {$game_result[0]}, $this -> {$game_result[2]})
+        );
+    }
 
-        return $this -> game_points[$you][$me];
+    private function scores($result) {
+        return match($result) {
+            'rock|rock'         => $this -> rock_score      + $this -> draw_score, // Rock covered by Paper
+            'rock|paper'        => $this -> paper_score     + $this -> win_score, // Paper covers rock
+            'rock|scissors'     => $this -> scissors_score  + $this -> loss_score, // Rock blunts scissors
+            'paper|rock'        => $this -> rock_score      + $this -> loss_score, // Paper covers rock
+            'paper|paper'       => $this -> paper_score     + $this -> draw_score, // Draw
+            'paper|scissors'    => $this -> scissors_score  + $this -> win_score, // Scissors cuts paper
+            'scissors|rock'     => $this -> rock_score      + $this -> win_score, // Paper covers rock
+            'scissors|paper'    => $this -> paper_score     + $this -> loss_score, // Scissors cuts paper
+            'scissors|scissors' => $this -> scissors_score  + $this -> draw_score, // Draw],
+        };
     }
 }
 
